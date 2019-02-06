af3d6b33688b63a1fb7f4a97600fa9534611c02c
Simon Bang <bangtsimon@gmail.com>
2019-02-05 22:02:48 -0800

    added files

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 
-import logo from "./logo.svg";
 import "./style.css";
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div className={classnames("App", this.props.className)}>
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
+				<div className="project">
+					Movie Shopper
 				</div>
-				<p className="App-intro">
-					To get started, edit <code>./components/Home/index.js</code> and save to reload.
-				</p>
+				<div className="project">
+					Web Crawler
+				</div>
+				<div className="project">
+					Bibimbap King
+				</div>
+				<div className="project">
+					Project #4
+				</div>
 			</div>
 		);
 	}
--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import classnames from "classnames";
-
+import logo from "../logo.svg";
 import { IndexLink } from "react-router";
 import NavLink from "../NavLink";
+import "./style.css";
 
 export default class Header extends Component {
 	render() {
 		return(
 			<header className={classnames("Header", this.props.className)}>
-				<h1>Header</h1>
-				<p>TODO: UPDATE Header <code>./components/Layout/Header/index.js</code></p>
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Youth Education on Mental Illness</h2>
+				</div>
 				<nav>
 					<ul>
 						<li><IndexLink to="/" activeClassName="active">home</IndexLink></li>
